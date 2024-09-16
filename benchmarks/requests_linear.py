2bfac2b5b36664d95fd4fa177632f2726e953a09
wnma3mz <wnma3mz@gmail.com>
2024-09-16 18:49:56 +0800

    add rpc benchmarks

--- benchmarks/requests_linear.py
+++ benchmarks/requests_linear.py
@@ -2,7 +2,15 @@ import requests
 import torch
 import time
 
-if __name__ == "__main__":
+from typing import *
+
+import grpc
+
+from rpc_comm import schemas_pb2, schemas_pb2_grpc
+from rpc_comm.convert import list_to_protobuf, protobuf_to_list
+
+
+def http_func():
     x = torch.randn(1, 4096).tolist()
     headers = {
         "accept": "application/json",
@@ -13,3 +21,26 @@ if __name__ == "__main__":
     s = requests.post("http://localhost:8003/forward", headers=headers, json=json_data)
     print("Cost time:", time.time() - s1)
     print(s.json()["cost_time"])
+
+
+class RPCServer:
+    def __init__(self, url_list: List[str]):
+        self.stub_list = []
+        url = "localhost:8004"
+        channel = grpc.insecure_channel(url)
+        self.stub_list.append(schemas_pb2_grpc.RPCServiceStub(channel))
+
+    def forward(self, stub, data):
+        x = torch.randn(1, 4096).tolist()
+        s1 = time.time()
+        request = schemas_pb2.ForwardData(
+            uuid="1234t", hidden_states=list_to_protobuf(x)
+        )
+        out = stub.Forward(request)
+        print("Cost time:", time.time() - s1)
+        print("Output:", out.cost_time)
+
+
+if __name__ == "__main__":
+    rpc_server = RPCServer(["localhost:8004"])
+    rpc_server.forward(rpc_server.stub_list[0], {})
