a6294c37e81d0654abdadec7a6eb76a496e6b6ae
lujianghu <lujianghu@kanyun.com>
2024-11-15 19:23:06 +0800

    fix benchmarks run_engine

--- benchmarks/run_engine.py
+++ benchmarks/run_engine.py
@@ -1,61 +1,32 @@
 import asyncio
 from dataclasses import dataclass
-import os
+import logging
 
-from tllm.engine import SequenceRequestData
-from tllm.entrypoints.protocol import random_uuid
-from tllm.generate.sampler_utils import SamplerUtils, SamplingParams
-from tllm.rpc.handler import run
-from tllm.utils import init_engine
+from tllm.entrypoints.protocol import ChatCompletionRequest
+from tllm.entrypoints.server_chat import OpenAIServing
+from tllm.utils import init_engine, setup_logger
 
 
 @dataclass
 class Args:
     model_path: str = "/Users/lujianghu/Documents/Llama-3.2-1B-Instruct"
-    config_path: str = "./examples/config.json"
-
-
-@dataclass
-class ClientArgs:
-    host: str = "0.0.0.0"
-    port: int = 50051
-    master_url: str = "ws://localhost:8000"
-    start_layer_idx: int = 0
-    end_layer_idx: int = 16
-    pp_rank: int = 0
-    model_path: str = "/Users/jianghulu/Documents/Llama-3.2-1B-Instruct-bf16"
-    ip_addr: str = "localhost"
-
-
-async def fetch_result(result_generator: "AsyncIterator"):
-    async for res in result_generator:
-        print("=" * 20)
-        final_res = res
-
-    print(final_res)
+    is_local: bool = True
+    is_debug: bool = True
 
 
 async def main():
-    client_args = ClientArgs()
-    run(client_args, True)
     args = Args()
-    engine, tok = init_engine(args)
+
+    logger = setup_logger(__name__, logging.DEBUG if args.is_debug else logging.INFO)
+    engine, tok, _ = await init_engine(args.model_path, args.is_local, logger)
+    _ = await engine.start()
     messages = [{"role": "user", "content": "Hello, how are you?"}]
-    input_ids = tok.preprocess(messages=messages).input_ids
 
-    print("input_ids: ", input_ids)
+    openai_serving_chat = OpenAIServing(engine, tok, args)
 
-    request_id = f"chat-{random_uuid()}"
-    sequence_data = SequenceRequestData(
-        request_id=request_id,
-        input_ids=input_ids,
-        sampler=SamplerUtils("greedy", tok),
-        sampling_params=SamplingParams(max_tokens=16),
-    )
-    result_generator = await engine.generate(sequence_data)
-
-    # result_generator = engine.generate_stream(sequence_data)
-    # await fetch_result(result_generator)
+    request = ChatCompletionRequest(model="test", messages=messages)
+    generator = await openai_serving_chat.create_chat_completion(request, None)
+    print(generator)
 
 
 if __name__ == "__main__":
