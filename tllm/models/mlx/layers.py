e22bf0f8f9fc7036661bf3ebc2727453720248ed
wnma3mz <wnma3mz@gmail.com>
2025-05-02 09:34:26 +0800

    move parallel layer to new file

--- tllm/models/mlx/layers.py
+++ tllm/models/mlx/layers.py
@@ -1,4 +1,3 @@
-import itertools
 from typing import List
 
 import mlx.core as mx
@@ -6,59 +5,7 @@ import mlx.nn as nn
 from mlx_lm.models.llama import ModelArgs, TransformerBlock, initialize_rope
 
 from tllm.commons.cache import AttentionData, RequestsCache, cat_func
-
-
-class BaseParallelLayer(nn.Module):
-    def __init__(self, world_size: int, rank: int) -> None:
-        self.world_size = world_size
-        self.rank = rank
-        super().__init__()
-
-
-class MergeParallelLayer(BaseParallelLayer):
-    def __init__(
-        self, row_size: int, col_size: int, dup_layer: int, world_size: int, rank: int, bias: bool = False
-    ) -> None:
-        super().__init__(world_size, rank)
-        assert col_size % self.world_size == 0
-        self.row_size, self.col_size = row_size, col_size
-        self.dup_layer = dup_layer
-        self.layer = nn.Linear(row_size, col_size * self.dup_layer // self.world_size, bias=bias)
-        self.ind = [i * col_size // self.world_size for i in range(1, self.dup_layer)]
-
-    def __call__(self, x: mx.array) -> List[mx.array]:
-        node_output = self.layer(x)
-        return (node_output[:, : self.ind[0]], node_output[:, self.ind[0] :])
-
-
-class QKVParallelLayer(BaseParallelLayer):
-    def __init__(self, row_size: int, col_size_list: List[int], world_size: int, rank: int, bias: bool = False) -> None:
-        super().__init__(world_size, rank)
-        for x in col_size_list:
-            assert x % self.world_size == 0
-        col_size = sum(col_size_list)
-        assert col_size % self.world_size == 0
-
-        self.row_size, self.col_size = row_size, col_size
-        self.col_size_list = [x // self.world_size for x in col_size_list]
-        self.layer = nn.Linear(row_size, col_size // self.world_size, bias=bias)
-        self.ind = list(itertools.accumulate(self.col_size_list[:-1]))
-
-    def __call__(self, x: mx.array) -> List[mx.array]:
-        node_output = self.layer(x)
-        return mx.split(node_output, self.ind, axis=-1)
-
-
-class RowParallelLayer(BaseParallelLayer):
-    def __init__(self, row_size: int, col_size: int, world_size: int, rank: int, bias: bool = False) -> None:
-        super().__init__(world_size, rank)
-        assert row_size % self.world_size == 0
-        self.row_size, self.col_size = row_size, col_size
-        self.layer = nn.Linear(row_size // self.world_size, col_size, bias=bias)
-        self.ind = [i * row_size // self.world_size for i in range(1, self.world_size)]
-
-    def __call__(self, x: mx.array) -> mx.array:
-        return self.layer(x)
+from tllm.models.mlx.parallel_layer import MergeParallelLayer, QKVParallelLayer, RowParallelLayer
 
 
 def rotate_half(x):
